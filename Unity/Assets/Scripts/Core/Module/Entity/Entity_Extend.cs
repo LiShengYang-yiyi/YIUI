4534d5ca1dfaab83f585581ffe67cff991154dc8
李胜扬 <379338943@qq.com>
2023-11-08 18:12:33 +0800

    扩展了一个有日志输出的方法方便调试

--- Unity/Assets/Scripts/Core/Module/Entity/Entity_Extend.cs
+++ Unity/Assets/Scripts/Core/Module/Entity/Entity_Extend.cs
@@ -27,5 +27,31 @@ namespace ET
             EventSystem.Instance.Awake(component,a);
             return component;
         }
+        
+        //自定义扩展 获取不到就报错 不管你log是true 还是false
+        //不想报错就别调用这个方法
+        public K GetComponent<K>(bool log) where K : Entity
+        {
+            if (this.components == null)
+            {
+                Log.Error($"错误 this.components == null");
+                return null;
+            }
+
+            Entity component;
+            if (!this.components.TryGetValue(typeof (K), out component))
+            {
+                Log.Error($"{this.GetType().Name} 目标没有这个组件 {typeof (K).Name}");
+                return default;
+            }
+
+            // 如果有IGetComponent接口，则触发GetComponentSystem
+            if (this is IGetComponent)
+            {
+                EventSystem.Instance.GetComponent(this, component);
+            }
+
+            return (K)component;
+        }
     }
 }
