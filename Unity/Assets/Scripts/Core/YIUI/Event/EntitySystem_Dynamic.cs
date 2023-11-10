3dfae6cbf4bf4ea640dee1fa1ea38cd275f90f4a
李胜扬 <379338943@qq.com>
2023-11-10 17:51:59 +0800

    ET源码扩展   新的事件  UI事件与全局事件

---
+++ Unity/Assets/Scripts/Core/YIUI/Event/EntitySystem_Dynamic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ET
+{
+    public partial class EntitySystem
+    {
+        public Queue<EntityRef<Entity>>[] Queues => queues;
+        
+        public async ETTask DynamicEvent<P1>(P1 message) where P1 : struct
+        {
+            using ListComponent<ETTask> list = ListComponent<ETTask>.Create();
+
+            var queue = queues[InstanceQueueIndex.Dynamic];
+            int count = queue.Count;
+
+            while (count-- > 0)
+            {
+                Entity component = queue.Dequeue();
+                if (component == null)
+                {
+                    continue;
+                }
+
+                if (component.IsDisposed)
+                {
+                    continue;
+                }
+
+                queue.Enqueue(component);
+
+                List<object> iEventSystems =
+                        EntitySystemSingleton.Instance.TypeSystems.GetSystems(component.GetType(), typeof (IDynamicEvent<P1>));
+                if (iEventSystems == null)
+                {
+                    continue;
+                }
+                
+                foreach (IDynamicEventSystem<P1> iEventSystem in iEventSystems)
+                {
+                    list.Add(iEventSystem.Run(component, message));
+                }
+            }
+
+            try
+            {
+                await ETTaskHelper.WaitAll(list);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e);
+            }
+        }
+    }
+}
