3c1641c9cefab6dabb6f5d0c6426ba93c5270da7
Lsy <379338943@qq.com>
2024-02-01 23:25:21 +0800

    自动创建一个MainPanel  自动生成无报错

---
+++ Unity/Assets/Scripts/HotfixView/Client/YIUISystem/Main/MainPanelComponentSystem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using YIUIFramework;
+using System.Collections.Generic;
+
+namespace ET.Client
+{
+    /// <summary>
+    /// Author  YIUI
+    /// Date    2024.2.1
+    /// Desc
+    /// </summary>
+    [FriendOf(typeof(MainPanelComponent))]
+    public static partial class MainPanelComponentSystem
+    {
+        [EntitySystem]
+        private static void YIUIInitialize(this MainPanelComponent self)
+        {
+        }
+        
+        [EntitySystem]
+        private static void Awake(this MainPanelComponent self)
+        {
+        }
+        
+        [EntitySystem]
+        private static void Destroy(this MainPanelComponent self)
+        {
+        }
+        
+        [EntitySystem]
+        private static async ETTask<bool> YIUIOpen(this MainPanelComponent self)
+        {
+            await ETTask.CompletedTask;
+            return true;
+        }
+        
+        #region YIUIEvent开始
+        #endregion YIUIEvent结束
+    }
+}
---
+++ Unity/Assets/Scripts/ModelView/Client/YIUIComponent/Main/MainPanelComponent.cs
@@ -0,0 +1,14 @@
+﻿using YIUIFramework;
+
+namespace ET.Client
+{
+    /// <summary>
+    /// Author  YIUI
+    /// Date    2024.2.1
+    /// Desc
+    /// </summary>
+    public partial class MainPanelComponent : Entity
+    {
+
+    }
+}
