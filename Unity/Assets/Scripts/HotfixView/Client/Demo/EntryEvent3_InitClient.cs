b7cd271ec348ae9158f8e72087580feaaae9f907
Lsy <379338943@qq.com>
2024-02-02 11:23:54 +0800

    ET UI流程

--- Unity/Assets/Scripts/HotfixView/Client/Demo/EntryEvent3_InitClient.cs
+++ Unity/Assets/Scripts/HotfixView/Client/Demo/EntryEvent3_InitClient.cs
@@ -16,12 +16,19 @@ namespace ET.Client
             root.AddComponent<ResourcesLoaderComponent>();
             root.AddComponent<PlayerComponent>();
             root.AddComponent<CurrentScenesComponent>();
-
-            await root.AddComponent<YIUIMgrComponent>().Initialize();
             
             // 根据配置修改掉Main Fiber的SceneType
             SceneType sceneType = EnumHelper.FromString<SceneType>(globalComponent.GlobalConfig.AppType.ToString());
             root.SceneType = sceneType;
+            
+            await root.AddComponent<YIUIMgrComponent>().Initialize();
+            #region 根据需求自行处理
+            //在editor下自动打开  也可以根据各种外围配置 或者 GM等级打开
+            if (Define.IsEditor)
+            {
+                root.AddComponent<GMCommandComponent>();
+            }
+            #endregion
             
             await EventSystem.Instance.PublishAsync(root, new AppStartInitFinish());
         }
