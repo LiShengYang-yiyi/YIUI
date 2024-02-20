ef0acd3324a561336b3dd7da21477459b59f62a0
Lsy <379338943@qq.com>
2024-02-20 20:45:59 +0800

    修改配置加载器  契合luban

--- Unity/Assets/Scripts/Loader/ConfigLoaderInvoker.cs
+++ Unity/Assets/Scripts/Loader/ConfigLoaderInvoker.cs
@@ -1,3 +1,4 @@
+using Luban;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -6,51 +7,43 @@ using UnityEngine;
 namespace ET
 {
     [Invoke]
-    public class GetAllConfigBytes: AInvokeHandler<ConfigLoader.GetAllConfigBytes, ETTask<Dictionary<Type, byte[]>>>
+    public class GetAllConfigBytes : AInvokeHandler<ConfigLoader.GetAllConfigBytes, ETTask<Dictionary<Type, ByteBuf>>>
     {
-        public override async ETTask<Dictionary<Type, byte[]>> Handle(ConfigLoader.GetAllConfigBytes args)
+        public override async ETTask<Dictionary<Type, ByteBuf>> Handle(ConfigLoader.GetAllConfigBytes args)
         {
-            Dictionary<Type, byte[]> output = new Dictionary<Type, byte[]>();
-            HashSet<Type> configTypes = CodeTypes.Instance.GetTypes(typeof (ConfigAttribute));
-            
+            Dictionary<Type, ByteBuf> output = new Dictionary<Type, ByteBuf>();
+            HashSet<Type> configTypes = CodeTypes.Instance.GetTypes(typeof(ConfigAttribute));
+
             if (Define.IsEditor)
             {
                 string ct = "cs";
                 GlobalConfig globalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
                 CodeMode codeMode = globalConfig.CodeMode;
-                switch (codeMode)
+                ct = codeMode switch
                 {
-                    case CodeMode.Client:
-                        ct = "c";
-                        break;
-                    case CodeMode.Server:
-                        ct = "s";
-                        break;
-                    case CodeMode.ClientServer:
-                        ct = "cs";
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                List<string> startConfigs = new List<string>()
+                    CodeMode.Client => "c",
+                    CodeMode.Server => "s",
+                    CodeMode.ClientServer => "cs",
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+
+                List<string> startConfigs = new()
                 {
-                    "StartMachineConfigCategory", 
-                    "StartProcessConfigCategory", 
-                    "StartSceneConfigCategory", 
-                    "StartZoneConfigCategory",
+                    "StartMachineConfigCategory", "StartProcessConfigCategory", "StartSceneConfigCategory", "StartZoneConfigCategory"
                 };
                 foreach (Type configType in configTypes)
                 {
                     string configFilePath;
                     if (startConfigs.Contains(configType.Name))
                     {
-                        configFilePath = $"../Config/Excel/{ct}/{Options.Instance.StartConfig}/{configType.Name}.bytes";    
+                        configFilePath = $"../Config/Excel/{ct}/{Options.Instance.StartConfig}/{configType.Name}.bytes";
                     }
                     else
                     {
                         configFilePath = $"../Config/Excel/{ct}/{configType.Name}.bytes";
                     }
-                    output[configType] = File.ReadAllBytes(configFilePath);
+
+                    output[configType] = new ByteBuf(File.ReadAllBytes(configFilePath));
                 }
             }
             else
@@ -58,50 +51,41 @@ namespace ET
                 foreach (Type type in configTypes)
                 {
                     TextAsset v = await ResourcesComponent.Instance.LoadAssetAsync<TextAsset>($"Assets/Bundles/Config/{type.Name}.bytes");
-                    output[type] = v.bytes;
+                    output[type] = new ByteBuf(v.bytes);
                 }
             }
 
             return output;
         }
     }
-    
+
     [Invoke]
-    public class GetOneConfigBytes: AInvokeHandler<ConfigLoader.GetOneConfigBytes, ETTask<byte[]>>
+    public class GetOneConfigBytes : AInvokeHandler<ConfigLoader.GetOneConfigBytes, ETTask<ByteBuf>>
     {
-        public override async ETTask<byte[]> Handle(ConfigLoader.GetOneConfigBytes args)
+        public override async ETTask<ByteBuf> Handle(ConfigLoader.GetOneConfigBytes args)
         {
             string ct = "cs";
             GlobalConfig globalConfig = Resources.Load<GlobalConfig>("GlobalConfig");
             CodeMode codeMode = globalConfig.CodeMode;
-            switch (codeMode)
+            ct = codeMode switch
             {
-                case CodeMode.Client:
-                    ct = "c";
-                    break;
-                case CodeMode.Server:
-                    ct = "s";
-                    break;
-                case CodeMode.ClientServer:
-                    ct = "cs";
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-            List<string> startConfigs = new List<string>()
+                CodeMode.Client => "c",
+                CodeMode.Server => "s",
+                CodeMode.ClientServer => "cs",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            List<string> startConfigs = new()
             {
-                "StartMachineConfigCategory", 
-                "StartProcessConfigCategory", 
-                "StartSceneConfigCategory", 
-                "StartZoneConfigCategory",
+                "StartMachineConfigCategory", "StartProcessConfigCategory", "StartSceneConfigCategory", "StartZoneConfigCategory"
             };
 
             string configName = args.ConfigName;
-                
+
             string configFilePath;
             if (startConfigs.Contains(configName))
             {
-                configFilePath = $"../Config/Excel/{ct}/{Options.Instance.StartConfig}/{configName}.bytes";    
+                configFilePath = $"../Config/Excel/{ct}/{Options.Instance.StartConfig}/{configName}.bytes";
             }
             else
             {
@@ -109,7 +93,7 @@ namespace ET
             }
 
             await ETTask.CompletedTask;
-            return File.ReadAllBytes(configFilePath);
+            return new ByteBuf(File.ReadAllBytes(configFilePath));
         }
     }
 }
