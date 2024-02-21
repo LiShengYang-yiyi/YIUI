08b83e2538500706d6484b741a03397b022b9075
Lsy <379338943@qq.com>
2024-02-21 11:31:49 +0800

    漏掉的配置修改 会导致服务器无法登陆

--- Unity/Assets/Scripts/Model/Share/Module/AI/AIConfig.cs
+++ Unity/Assets/Scripts/Model/Share/Module/AI/AIConfig.cs
@@ -1,33 +1,31 @@
 using System.Collections.Generic;
-using System.ComponentModel;
-using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace ET
 {
-	public partial class AIConfigCategory
-	{
-		[BsonIgnore]
-		public Dictionary<int, SortedDictionary<int, AIConfig>> AIConfigs = new Dictionary<int, SortedDictionary<int, AIConfig>>();
+    public partial class AIConfigCategory
+    {
+        [BsonIgnore]
+        public Dictionary<int, SortedDictionary<int, AIConfig>> AIConfigs = new();
 
-		public SortedDictionary<int, AIConfig> GetAI(int aiConfigId)
-		{
-			return this.AIConfigs[aiConfigId];
-		}
+        public SortedDictionary<int, AIConfig> GetAI(int aiConfigId)
+        {
+            return this.AIConfigs[aiConfigId];
+        }
 
-		partial void PostInit()
-		{
-			foreach (var kv in this.DataMap)
-			{
-				SortedDictionary<int, AIConfig> aiNodeConfig;
-				if (!this.AIConfigs.TryGetValue(kv.Value.AIConfigId, out aiNodeConfig))
-				{
-					aiNodeConfig = new SortedDictionary<int, AIConfig>();
-					this.AIConfigs.Add(kv.Value.AIConfigId, aiNodeConfig);
-				}
-				
-				aiNodeConfig.Add(kv.Key, kv.Value);
-			}
-		}
-	}
+        partial void PostInit()
+        {
+            foreach (var kv in this.DataMap)
+            {
+                SortedDictionary<int, AIConfig> aiNodeConfig;
+                if (!this.AIConfigs.TryGetValue(kv.Value.AIConfigId, out aiNodeConfig))
+                {
+                    aiNodeConfig = new SortedDictionary<int, AIConfig>();
+                    this.AIConfigs.Add(kv.Value.AIConfigId, aiNodeConfig);
+                }
+
+                aiNodeConfig.Add(kv.Key, kv.Value);
+            }
+        }
+    }
 }
