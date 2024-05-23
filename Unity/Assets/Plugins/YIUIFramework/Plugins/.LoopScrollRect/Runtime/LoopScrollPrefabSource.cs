7832938130438a61a2eb2d709bfc9c1865338e4c
Lsy <379338943@qq.com>
2024-05-23 17:40:56 +0800

    新增异步 无限循环列表

---
+++ Unity/Assets/Plugins/YIUIFramework/Plugins/.LoopScrollRect/Runtime/LoopScrollPrefabSource.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using ET;
+
+namespace UnityEngine.UI
+{
+    public interface LoopScrollPrefabAsyncSource
+    {
+        ETTask<GameObject> GetObject(int index);
+
+        void ReturnObject(Transform trans);
+    }
+}
