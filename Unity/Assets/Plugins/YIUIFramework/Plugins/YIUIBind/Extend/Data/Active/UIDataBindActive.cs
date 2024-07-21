3b1091ca4fcf915c44654bf22dc23a663c4175d7
Lsy <379338943@qq.com>
2024-07-21 22:04:48 +0800

    修复动画问题

--- Unity/Assets/Plugins/YIUIFramework/Plugins/YIUIBind/Extend/Data/Active/UIDataBindActive.cs
+++ Unity/Assets/Plugins/YIUIFramework/Plugins/YIUIBind/Extend/Data/Active/UIDataBindActive.cs
@@ -6,7 +6,7 @@ namespace YIUIFramework
 {
     [LabelText("GameObject的显隐")]
     [AddComponentMenu("YIUIBind/Data/显隐 【Active】 UIDataBindActive")]
-    public sealed class UIDataBindActive: UIDataBindBool
+    public sealed class UIDataBindActive : UIDataBindBool
     {
         [SerializeField]
         [LabelText("过度类型")]
@@ -15,7 +15,7 @@ namespace YIUIFramework
         [SerializeField]
         [LabelText("过度时间")]
         [HideIf("m_TransitionMode", UITransitionModeEnum.Instant)]
-        private float m_TransitionTime = 0.1f;
+        private float m_TransitionTime = 0.5f;
 
         private CanvasGroup m_CanvasGroup;
 
@@ -39,58 +39,53 @@ namespace YIUIFramework
 
                 if (m_CanvasGroup != null)
                 {
-                    if (gameObject.activeInHierarchy)
-                    {
-                        StopAllCoroutines();
+                    StopAllCoroutines();
 
-                        switch (m_TransitionMode)
-                        {
-                            case UITransitionModeEnum.Fade:
-                                if (result)
-                                {
-                                    gameObject.SetActive(true);
-                                    StartCoroutine(TransitionFade(m_CanvasGroup, 1.0f, true));
-                                }
-                                else
-                                {
-                                    StartCoroutine(TransitionFade(m_CanvasGroup, 0.0f, false));
-                                }
-                                break;
-                            case UITransitionModeEnum.FadeIn:
-                                if (result)
-                                {
-                                    gameObject.SetActive(true);
-                                    m_CanvasGroup.alpha = 0;
-                                    StartCoroutine(TransitionFade(m_CanvasGroup, 1.0f, true));
-                                }
-                                else
-                                {
-                                    gameObject.SetActive(false);
-                                }
-
-                                break;
-                            case UITransitionModeEnum.FadeOut:
-                                if (result)
-                                {
-                                    m_CanvasGroup.alpha = 1f;
-                                    gameObject.SetActive(true);
-                                }
-                                else
-                                {
-                                    m_CanvasGroup.alpha = 1f;
-                                    StartCoroutine(TransitionFade(m_CanvasGroup, 0.0f, false));
-                                }
-                                break;
-                            case UITransitionModeEnum.Instant:
-                            default:
-                                gameObject.SetActive(result);
-                                Debug.LogError($"不支持的功能 {m_TransitionMode}");
-                                break;
-                        }
-                    }
-                    else
+                    switch (m_TransitionMode)
                     {
-                        gameObject.SetActive(result);
+                        case UITransitionModeEnum.Fade:
+                            if (result)
+                            {
+                                gameObject.SetActive(true);
+                                StartCoroutine(TransitionFade(m_CanvasGroup, 1.0f, true));
+                            }
+                            else
+                            {
+                                StartCoroutine(TransitionFade(m_CanvasGroup, 0.0f, false));
+                            }
+
+                            break;
+                        case UITransitionModeEnum.FadeIn:
+                            if (result)
+                            {
+                                gameObject.SetActive(true);
+                                m_CanvasGroup.alpha = 0;
+                                StartCoroutine(TransitionFade(m_CanvasGroup, 1.0f, true));
+                            }
+                            else
+                            {
+                                gameObject.SetActive(false);
+                            }
+
+                            break;
+                        case UITransitionModeEnum.FadeOut:
+                            if (result)
+                            {
+                                m_CanvasGroup.alpha = 1f;
+                                gameObject.SetActive(true);
+                            }
+                            else
+                            {
+                                m_CanvasGroup.alpha = 1f;
+                                StartCoroutine(TransitionFade(m_CanvasGroup, 0.0f, false));
+                            }
+
+                            break;
+                        case UITransitionModeEnum.Instant:
+                        default:
+                            gameObject.SetActive(result);
+                            Debug.LogError($"不支持的功能 {m_TransitionMode}");
+                            break;
                     }
                 }
                 else
@@ -100,8 +95,7 @@ namespace YIUIFramework
             }
         }
 
-        private IEnumerator TransitionFade(
-        CanvasGroup group, float alphaTarget, bool activeTarget)
+        private IEnumerator TransitionFade(CanvasGroup group, float alphaTarget, bool activeTarget)
         {
             var leftTime   = m_TransitionTime;
             var alphaStart = group.alpha;
@@ -110,8 +104,8 @@ namespace YIUIFramework
                 yield return null;
                 leftTime -= Time.deltaTime;
                 var alpha = Mathf.Lerp(alphaStart,
-                    alphaTarget,
-                    1.0f - (leftTime / m_TransitionTime));
+                                       alphaTarget,
+                                       1.0f - (leftTime / m_TransitionTime));
                 group.alpha = alpha;
             }
 
