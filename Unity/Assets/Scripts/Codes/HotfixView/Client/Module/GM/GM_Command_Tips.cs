68ad19d2ed33dbc00ca9e16292ec9d5d5c64a4d4
Lsy <379338943@qq.com>
2024-12-16 13:55:54 +0800

    修改demo中的tips错误

--- Unity/Assets/Scripts/Codes/HotfixView/Client/Module/GM/GM_Command_Tips.cs
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Module/GM/GM_Command_Tips.cs
@@ -17,7 +17,8 @@ namespace ET.Client
 
         public async ETTask<bool> Run(Scene clientScene, ParamVo paramVo)
         {
-            TipsHelper.OpenSync<MessageTipsViewComponent>(paramVo);
+            var message = paramVo.Get<string>();
+            TipsHelper.OpenSync<MessageTipsViewComponent>(message);
             await ETTask.CompletedTask;
             return true;
         }
@@ -116,7 +117,8 @@ namespace ET.Client
 
         public async ETTask<bool> Run(Scene clientScene, ParamVo paramVo)
         {
-            TipsHelper.OpenSync<TextTipsViewComponent>(paramVo);
+            var message = paramVo.Get<string>();
+            TipsHelper.OpenSync<TextTipsViewComponent>(message);
             await ETTask.CompletedTask;
             return true;
         }
